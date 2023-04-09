b2429fc47a5d3767c3e49a8b8aade1eeeb2283c8
Bavfalcon9 <olybear9@gmail.com>
2023-04-09 15:28:36 -0500

    chore: add bc

---
+++ src/interfaces.rs
@@ -0,0 +1,133 @@
+use crate::io::{ByteReader, ByteWriter};
+
+/// Allows you to read from a `ByteReader` without needing to know the type.
+///
+/// ```no_run
+/// use binary_utils::io::{ByteReader, Reader};
+///
+/// pub struct MyStruct {
+///    pub a: u8,
+///    pub b: u8
+/// }
+///
+/// impl Reader for MyStruct {
+///     fn read(&self, buf: &mut ByteReader) -> Result<Self, std::io::Error> {
+///        let a = buf.read_u8()?;
+///        let b = buf.read_u8()?;
+///        Ok(Self { a, b })
+///     }
+/// }
+/// ```
+pub trait Reader<T> {
+    /// Reads `Self` from a `ByteReader`.
+    ///
+    /// For automatic implementations, use `#[derive(BinaryDecoder]` macro.
+    fn read(buf: &mut ByteReader) -> Result<T, std::io::Error>;
+}
+
+pub trait Writer {
+    /// Writes `Self` to a `ByteWriter`.
+    ///
+    /// For automatic implementations, use `#[derive(BinaryEncoder]` macro.
+    fn write(&self, buf: &mut ByteWriter) -> Result<&mut ByteWriter, std::io::Error>;
+
+    /// This is a utility function to write `Self` to a `ByteWriter` without
+    /// needing to create a `ByteWriter` first.
+    fn init_write(&self) -> Result<&mut ByteWriter, std::io::Error> {
+        let mut buf = ByteWriter::new();
+        self.write(&mut buf)
+    }
+}
+
+/// ## Deprecated
+/// __**This trait exists only for backwards compatibility.**__
+///
+/// If you wish to read and write from a `ByteReader` or `ByteWriter`,
+/// use the `Reader` and `Writer` traits.
+///
+/// ### New Implementation Example
+/// ```no_run
+/// use binary_utils::io::{ByteReader, ByteWriter};
+/// use binary_utils::interfaces::{Reader, Writer};
+///
+/// pub struct MyStruct;
+///
+/// impl Reader for MyStruct;
+/// impl Writer for MyStruct;
+/// ```
+///
+/// ## `Streamable`
+/// A trait to parse and unparse header structs from a given buffer.
+///
+/// ```no_run
+/// use binary_utils::{Streamable, error::BinaryError};
+///
+/// struct Foo {
+///     bar: u8,
+///     foo_bar: u16
+/// }
+/// impl Streamable for Foo {
+///     fn parse(&self) -> Result<Vec<u8>, BinaryError> {
+///         use std::io::Write;
+///         let mut stream = Vec::<u8>::new();
+///         stream.write_all(&self.bar.parse()?[..])?;
+///         stream.write_all(&self.bar.parse()?[..])?;
+///         Ok(stream)
+///     }
+///
+///     fn compose(source: &[u8], position: &mut usize) -> Result<Self, BinaryError> {
+///         // Streamable is implemented for all primitives, so we can
+///         // just use this implementation to read our properties.
+///         Ok(Self {
+///             bar: u8::compose(&source, position)?,
+///             foo_bar: u16::compose(&source, position)?
+///         })
+///     }
+/// }
+/// ```
+pub trait Streamable<T>: Reader<T> + Writer {
+    /// Writes `self` to the given buffer.
+    fn parse(&self) -> Result<Vec<u8>, crate::error::BinaryError>
+    where
+        T: Sized
+    {
+        if let Ok(v) = self.init_write() {
+            Ok(v.as_slice().to_vec())
+        } else {
+            Err(crate::error::BinaryError::RecoverableUnknown)
+        }
+    }
+
+    /// Writes and unwraps `self` to the given buffer.
+    ///
+    /// ⚠️ This method is not fail safe, and will panic if result is Err.
+    fn fparse(&self) -> Vec<u8>
+    where
+        T: Sized
+    {
+        self.parse().unwrap()
+    }
+
+    /// Reads `self` from the given buffer.
+    fn compose(source: &[u8], position: &mut usize) -> Result<T, crate::error::BinaryError>
+    where
+        T: Sized
+    {
+        let mut reader = ByteReader::from(&source[*position..]);
+        if let Ok(v) = Self::read(&mut reader) {
+            Ok(v)
+        } else {
+            Err(crate::error::BinaryError::RecoverableUnknown)
+        }
+    }
+
+    /// Reads and unwraps `self` from the given buffer.
+    ///
+    /// ⚠️ This method is not fail safe, and will panic if result is Err.
+    fn fcompose(source: &[u8], position: &mut usize) -> T
+    where
+        T: Sized,
+    {
+        Self::compose(source, position).unwrap()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-/// The `ByteReader` and `ByteWriter` traits are used to read and write bytes from a buffer.
+pub mod interfaces;
+
 /// The io module contains implementations of these traits for `bytes::Buf` and `bytes::BufMut`.
 ///
 /// Example:
@@ -14,4 +15,64 @@
 /// ```
 pub mod io;
 pub mod pool;
-pub mod stream;
+
+/// This is a legacy module that will be removed in the future.
+/// This module has been replaced in favor of `std::io::Error`.
+///
+/// # This module is deprecated
+pub mod error {
+    /// An enum consisting of a Binary Error
+    /// (recoverable)
+    #[derive(Debug, PartialEq)]
+    pub enum BinaryError {
+        /// Offset is out of bounds
+        ///
+        /// **Tuple Values:**
+        /// - `usize` = Given Offset.
+        /// - `usize` = Stream length.
+        /// - `&'static str` = Message to add on to the error.
+        OutOfBounds(usize, usize, &'static str),
+
+        /// Similar to `OutOfBounds` except it means;
+        /// the stream tried to read more than possible.
+        ///
+        /// **Tuple Values:**
+        /// - `usize` = Stream length.
+        EOF(usize),
+
+        /// A known error that was recoverable to safely proceed the stack.
+        RecoverableKnown(String),
+
+        /// An unknown error occurred, but it wasn't critical,
+        /// we can safely proceed on the stack.
+        RecoverableUnknown,
+    }
+
+    impl BinaryError {
+        pub fn get_message(&self) -> String {
+            match self {
+                Self::OutOfBounds(offset, length, append) => {
+                    format!("Offset {} out of range for a buffer size with: {}. {}", offset, length, append)
+                },
+                Self::EOF(length) => format!("Buffer reached End Of File at offset: {}", length),
+                Self::RecoverableKnown(msg) => msg.clone(),
+                Self::RecoverableUnknown => "An interruption occurred when performing a binary operation, however this error was recovered safely.".to_string()
+            }
+        }
+    }
+
+    impl From<std::io::Error> for BinaryError {
+        fn from(_error: std::io::Error) -> Self {
+            Self::RecoverableUnknown
+        }
+    }
+
+    impl std::fmt::Display for BinaryError {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(f, "{}", self.get_message())
+        }
+    }
+
+}
+
+pub use interfaces::Streamable;
