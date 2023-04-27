ef73598d4f55f51c89706a820eecd8ef75edd334
Bavfalcon9 <olybear9@gmail.com>
2023-04-26 22:01:42 -0500

    feat: Working derive macros for named structs

--- codegen/src/io/mod.rs
+++ codegen/src/io/mod.rs
@@ -1,11 +1,17 @@
 pub(crate) mod enums;
 pub(crate) mod structs;
 pub(crate) mod unions;
+pub(crate) mod util;
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput, Data};
+use syn::{parse_macro_input, Data, DeriveInput};
 
-pub(crate) type AstContext<'a> = (&'a syn::Ident, &'a Vec<syn::Attribute>, &'a syn::Generics, &'a syn::Visibility);
+pub(crate) type AstContext<'a> = (
+    &'a syn::Ident,
+    &'a Vec<syn::Attribute>,
+    &'a syn::Generics,
+    &'a syn::Visibility,
+);
 
 // BinaryEncoder is a derive macro that implements `::binary_utils::interfaces::Reader<T>` and `::binary_utils::interfaces::Writer<T>`
 pub(crate) fn binary_encoder(input: TokenStream) -> TokenStream {
@@ -15,15 +21,9 @@ pub(crate) fn binary_encoder(input: TokenStream) -> TokenStream {
     let mut err = proc_macro2::TokenStream::new();
 
     let stream = match input.data {
-        Data::Struct(d) => {
-            structs::derive_struct(ctx, d, &mut err)
-        },
-        Data::Enum(d) => {
-            enums::derive_enum(ctx, d, &mut err)
-        },
-        Data::Union(d) => {
-            unions::derive_union(ctx, d, &mut err)
-        }
+        Data::Struct(d) => structs::derive_struct(ctx, d, &mut err),
+        Data::Enum(d) => enums::derive_enum(ctx, d, &mut err),
+        Data::Union(d) => unions::derive_union(ctx, d, &mut err),
     };
 
     if err.is_empty() {
