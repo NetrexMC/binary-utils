d8897f5e2a08c369b9ff50d8ad803057cc3b2a0f
Bavfalcon9 <olybear9@gmail.com>
2023-04-26 23:11:33 -0500

    doc: Add docs to derive macro

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -39,13 +39,119 @@ pub fn derive_stream(_input: TokenStream) -> TokenStream {
 }
 
 /// This proc-macro implements both the `Reader` and `Writer` traits from `binary_utils::interfaces`.
+/// It is important to note that not all attributes can be used on all types, and some attributes are exclusive to certain variants.
 ///
 /// ## Structs
-/// `BinaryIo` supports both Named, and Unnamed structs; however it does not support unit structs.
+/// `BinaryIo` supports both Named, and Unnamed structs. However, this derive macro does not support unit structs.
 /// This macro will encode/decode the fields of the struct in the order they are defined, as long as they are not skipped;
 /// however as an additional requirement, each field **MUST** implement** the `Reader` and `Writer` traits, if they do not, this macro will fail.
+///
+/// **Example:**
+/// The following example will provide both a `Reader` and `Writer` implementation for the struct `ABC`, where each field is encoded as it's respective
+/// type to the `Bytewriter`/`Bytereader`.
+/// ```ignore
+/// use binary_utils::interfaces::{Reader, Writer};
+/// use binary_utils::BinaryIo;
+///
+/// #[derive(BinaryIo, Debug)]
+/// struct ABC {
+///    a: u8,
+///    b: Option<u8>,
+///    c: u8,
+/// }
+///```
+///
+/// Sometimes it can be more optimal to use Unnamed fields, if you do not care about the field names, and only want to encode/decode the fields in the order they are defined.
+/// The behavior of this macro is the same as the previous example, except the fields are unnamed.
+/// ```ignore
+/// use binary_utils::interfaces::{Reader, Writer};
+/// use binary_utils::BinaryIo;
+///
+/// #[derive(BinaryIo, Debug)]
+/// struct ABC(u8, Option<u8>, u8);
+/// ```
+///
+/// ### Attributes
+/// Structs have a few exclusive attributes that can be used to control the encoding/decoding of the struct. <br />
+/// These attributes control and modify the behavior of the `BinaryIo` macro.
+/// <br /><br />
+///
+/// #### Skip
+/// The `#[skip]` attribute does as the name implies, and can be used to skip a field when encoding/decoding. <br />
+/// This attribute can be used on both named, and unnamed fields.
+/// **Syntax:**
+/// ```rust
+/// #[skip]
+/// ```
+///
+///
+/// **Example:**
+/// ```ignore
+/// use binary_utils::interfaces::{Reader, Writer};
+/// use binary_utils::BinaryIo;
+///
+/// #[derive(BinaryIo, Debug)]
+/// struct ABC {
+///     a: u8,
+///     #[skip]
+///     b: Option<u8>,
+///     c: u8
+/// }
+/// ```
+///
+/// #### Require
+/// This attribute explicitly requires a field to be present when either encoding, or decoding; and will fail if the field is not present. <br />
+/// This can be useful if you want to ensure that an optional field is present when encoding, or decoding it.
+///
+/// **Syntax:**
+/// ```rust
+/// #[require(FIELD)]
+/// ```
+///
+///
+/// **Example:**
+/// In the following example, `b` is explicitly required to be present when encoding, or decoding `ABC`, and it's value is not allowed to be `None`.
+/// ```ignore
+/// use binary_utils::interfaces::{Reader, Writer};
+/// use binary_utils::BinaryIo;
+///
+/// #[derive(BinaryIo, Debug)]
+/// struct ABC {
+///     a: u8,
+///     b: Option<u8>,
+///     #[require(b)]
+///     c: Option<u8>
+/// }
+/// ```
+///
+/// #### If Present
+/// This attribute functions identically to `#[require]`, however it does not fail if the field is not present.
+///
+/// #### Satisfy
+/// This attribute will fail if the expression provided does not evaluate to `true`. <br />
+/// This attribute can be used to ensure that a field is only encoded/decoded if a certain condition is met.
+/// This can be useful if you're sending something like `Authorization` or `Authentication` packets, and you want to ensure that the client is authenticated before
+/// sending the packet.
+///
+/// **Syntax:**
+///
+/// ```rust
+/// #[satisfy(EXPR)]
+/// ```
+/// **Example:**
+/// ```ignore
+/// #[derive(BinaryIo, Debug)]
+/// struct ABC {
+///     a: u8,
+///     #[satisfy(self.a == 10)]
+///     b: Option<u8>,
+///     c: u8,
+/// }
+/// ```
+/// ---
 /// 
-/// 
+/// ## Enums
+/// Enums function identically to structs, however they also 
 #[proc_macro_derive(BinaryIo, attributes(skip, require, if_present, satisfy))]
 pub fn derive_binary_io(input: TokenStream) -> TokenStream {
     io::binary_encoder(input)
