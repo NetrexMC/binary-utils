da4031c91fc33496ee7706e8360e857aca5c4455
Bavfalcon9 <olybear9@gmail.com>
2023-04-27 00:52:44 -0500

    feat: Add unnamed structs.

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -70,20 +70,30 @@ pub fn derive_stream(_input: TokenStream) -> TokenStream {
 /// #[derive(BinaryIo, Debug)]
 /// struct ABC(u8, Option<u8>, u8);
 /// ```
+/// ---
 ///
-/// ### Attributes
-/// Structs have a few exclusive attributes that can be used to control the encoding/decoding of the struct. <br />
+/// ## Enums
+/// Enums description has not been written as of yet...
+///
+/// ---
+///
+/// ## Attributes
+/// Structs and enums have a few exclusive attributes that can be used to control the encoding/decoding of the struct. <br />
 /// These attributes control and modify the behavior of the `BinaryIo` macro.
 /// <br /><br />
 ///
-/// #### Skip
+/// ### Skip
 /// The `#[skip]` attribute does as the name implies, and can be used to skip a field when encoding/decoding. <br />
-/// This attribute can be used on both named, and unnamed fields.
+///
 /// **Syntax:**
 /// ```rust
 /// #[skip]
 /// ```
 ///
+/// **Compatibility:**
+/// - ✅ Named Structs
+/// - ✅ Unnamed Structs
+/// - ✅ Enums
 ///
 /// **Example:**
 /// ```ignore
@@ -99,7 +109,7 @@ pub fn derive_stream(_input: TokenStream) -> TokenStream {
 /// }
 /// ```
 ///
-/// #### Require
+/// ### Require
 /// This attribute explicitly requires a field to be present when either encoding, or decoding; and will fail if the field is not present. <br />
 /// This can be useful if you want to ensure that an optional field is present when encoding, or decoding it.
 ///
@@ -108,6 +118,10 @@ pub fn derive_stream(_input: TokenStream) -> TokenStream {
 /// #[require(FIELD)]
 /// ```
 ///
+/// **Compatibility:**
+/// - ✅ Named Structs
+/// - ❌ Unnamed Structs
+/// - ✅ Enums
 ///
 /// **Example:**
 /// In the following example, `b` is explicitly required to be present when encoding, or decoding `ABC`, and it's value is not allowed to be `None`.
@@ -124,10 +138,10 @@ pub fn derive_stream(_input: TokenStream) -> TokenStream {
 /// }
 /// ```
 ///
-/// #### If Present
+/// ### If Present
 /// This attribute functions identically to `#[require]`, however it does not fail if the field is not present.
 ///
-/// #### Satisfy
+/// ### Satisfy
 /// This attribute will fail if the expression provided does not evaluate to `true`. <br />
 /// This attribute can be used to ensure that a field is only encoded/decoded if a certain condition is met.
 /// This can be useful if you're sending something like `Authorization` or `Authentication` packets, and you want to ensure that the client is authenticated before
@@ -138,6 +152,11 @@ pub fn derive_stream(_input: TokenStream) -> TokenStream {
 /// ```rust
 /// #[satisfy(EXPR)]
 /// ```
+/// **Compatibility:**
+/// - ✅ Named Structs
+/// - ❌ Unnamed Structs
+/// - ✅ Enums
+///
 /// **Example:**
 /// ```ignore
 /// #[derive(BinaryIo, Debug)]
@@ -149,9 +168,7 @@ pub fn derive_stream(_input: TokenStream) -> TokenStream {
 /// }
 /// ```
 /// ---
-/// 
-/// ## Enums
-/// Enums function identically to structs, however they also 
+///
 #[proc_macro_derive(BinaryIo, attributes(skip, require, if_present, satisfy))]
 pub fn derive_binary_io(input: TokenStream) -> TokenStream {
     io::binary_encoder(input)
